5274a0af0123588daaf097404ef250aed5e7ffdc
Avi-24505 <arnavagarwal245@gmail.com>
2024-10-01 15:06:43 +0530

    functionality done, project completed

---
+++ index.js
@@ -0,0 +1,14 @@
+const getcolor = ()=>{
+    const randomnumber = Math.floor(Math.random()*1677215);
+    const randomcode = "#"+ randomnumber.toString(16);
+    document.body.style.backgroundColor = randomcode;
+    document.getElementById("color-code").innerText = randomcode;
+}
+
+
+document.grtElementbyId("btn").addEventListener(
+    "click",
+    getcolor
+)
+
+getcolor();
